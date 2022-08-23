5d24133adac3918d406cb2d3a24581c8d750a860
SantiagoMartin1 <101400604+SantiagoMartin1@users.noreply.github.com>
2022-08-23 15:18:42 -0300

    #7

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 document.getElementById("btn__iniciar-sesion").addEventListener("click", iniciarSesion);
 document.getElementById("btn__registrarse").addEventListener("click", register);
 window.addEventListener("resize", anchoPage);
-
 //Declarando variables
 var formulario_login = document.querySelector(".formulario__login");
 var formulario_register = document.querySelector(".formulario__register");
@@ -11,6 +10,10 @@ var caja_trasera_login = document.querySelector(".caja__trasera-login");
 var caja_trasera_register = document.querySelector(".caja__trasera-register");
 
     //FUNCIONES
+
+    function myFunction() {
+        alert("I am an alert box!");
+    }
 
 function anchoPage(){
 
