bc328872ef801dd2df51966a2a4d4992f70fe1c7
SantiagoMartin1 <101400604+SantiagoMartin1@users.noreply.github.com>
2022-08-26 12:09:22 -0300

    supuestamente terminado el js de subir archivos

--- account.js
+++ account.js
@@ -16,12 +16,98 @@ document.addEventListener("DOMContentLoaded", () => {
     })
 
 //hacer el drag over
+    dropArea.addEventListener("dragover", (e) => {
+        e.preventDefault();
+        files = e.dataTrasnfer.files;
+        showFiles(files);
+        dropArea.classList.add("active");
+        dragText.textContent = "Suelta para subir los archivos"
+    });
 
-    dropArea.addEventListener("dragleave", (e => {
+    dropArea.addEventListener("dragleave", (e) => {
         e.preventDefault();
         dropArea.classList.remove("active");
         dragText.textContent = "Arrastra y suelta archivos";
     });
 
-    drop
+    dropArea.addEventListener("drop", (e) => {
+        e.preventDefault();
+        files = e.dataTrasnfer.files;
+        showFiles(files);
+        dropArea.classList.remove("active");
+        dragText.textContent = "Arrastra y suelta archivos";
+    });
+
+    function showFiles(files) {
+        if (files.length == undefined) {
+          processFile(files);
+        }
+        else {
+          for (const file of files) {
+            processFile(file);
+          }
+        }
+      }
+    
+      function processFile(file) {
+        const docType = file.type;
+        const validExtensions = ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'];
+      
+        if (validExtensions.includes(docType)) {
+          // archivo válido
+          const fileReader = new FileReader();
+          const id = `file-${Math.random().toString(32).substring(7)}`;
+      
+          fileReader.addEventListener('load', e => {
+            const fileUrl = fileReader.result;
+            const image = `
+                <div id= "${id}" class = "file-container">
+                  <img src = "${fileUrl}" alt="${file.name}" width="50">
+                  <div class="status">
+                    <span>${file.name}</span>
+                    <span class="status-text">
+                      Loading...
+                    </span>
+                  </div>
+                </div>
+              `;
+      
+            const html = document.querySelector("#preview").innerHTML;
+            document.querySelector('#preview').innerHTML = image + html;
+      
+          });
+      
+          fileReader.readAsDataURL(file);
+          uploadFile(file, id);
+        } else {
+          // no es un archivo válido
+          alert("No es un archivo válido");
+        }
+    
+      }
+    
+      
+    
+      function uploadFile(file) {
+        const formData = new formData();
+        formData.append("file", file);
+    
+        try{
+          const response = await fetch("http://localhost:3000/upload", {
+            method: "POST", 
+            body: formData,
+          });
+    
+          const responseText = await response.text();
+    
+          document.querySelector(
+            `#${id} .status-text`
+            ).innerHTML = `<span class="success">Archivo subido correctamente...</span>`;
+    
+        } catch(error) {
+          document.querySelector(
+            `#${id} .status-text`
+            ).innerHTML = `<span class="failure">El archivo no ha podido subirse...</span>`;
+        }
+      }
 })
