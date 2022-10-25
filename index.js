79f11bacdf267b8b020aad79bfffce95507bf8cd
SantiagoMartin1 <101400604+SantiagoMartin1@users.noreply.github.com>
2022-10-25 15:04:21 -0300

    commit

--- index.js
+++ index.js
@@ -64,3 +64,22 @@ anchoPage();
             caja_trasera_login.style.opacity = "1";
         }
 }
+
+    /*function animacion()
+    {
+        if(main-btn.onclick){
+            let start = Date.now();
+
+            let timer = setInterval(function() {
+              let timePassed = Date.now() - start;
+        
+              train.style.left = timePassed / 5 + 'px';
+        
+              if (timePassed > 2000) clearInterval(timer);
+        
+            }, 20);
+        } */
+    
+
+
+
