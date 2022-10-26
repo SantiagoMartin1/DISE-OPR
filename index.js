44182a7df63d7cd550bc598f5cb6109744260659
SantiagoMartin1 <101400604+SantiagoMartin1@users.noreply.github.com>
2022-10-25 21:57:10 -0300

    cambios hasta ahora

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 //Ejecutando funciones
-document.getElementById("main-btn").addEventListener("click", iniciarSesion);
-document.getElementById("main-btn").addEventListener("click", register);
+document.getElementById("btn__iniciar-sesion").addEventListener("click", iniciarSesion);
+document.getElementById("btn__registrarse").addEventListener("click", register);
 window.addEventListener("resize", anchoPage);
+
 //Declarando variables
 var formulario_login = document.querySelector(".formulario__login");
 var formulario_register = document.querySelector(".formulario__register");
@@ -11,20 +12,16 @@ var caja_trasera_register = document.querySelector(".caja__trasera-register");
 
     //FUNCIONES
 
-    function myFunction() {
-        alert("Iniciaste sesion " + $usu_login);
-    }
-
 function anchoPage(){
 
     if (window.innerWidth > 850){
-        caja_trasera_register.style.display = "flex";
-        caja_trasera_login.style.display = "flex";
+        caja_trasera_register.style.display = "block";
+        caja_trasera_login.style.display = "block";
     }else{
-        caja_trasera_register.style.display = "flex";
+        caja_trasera_register.style.display = "block";
         caja_trasera_register.style.opacity = "1";
         caja_trasera_login.style.display = "none";
-        formulario_login.style.display = "flex";
+        formulario_login.style.display = "block";
         contenedor_login_register.style.left = "0px";
         formulario_register.style.display = "none";   
     }
@@ -32,54 +29,54 @@ function anchoPage(){
 
 anchoPage();
 
+
     function iniciarSesion(){
         if (window.innerWidth > 850){
-            formulario_login.style.display = "flex";
+            formulario_login.style.display = "block";
             contenedor_login_register.style.left = "10px";
             formulario_register.style.display = "none";
             caja_trasera_register.style.opacity = "1";
             caja_trasera_login.style.opacity = "0";
         }else{
-            formulario_login.style.display = "flex";
+            formulario_login.style.display = "block";
             contenedor_login_register.style.left = "0px";
             formulario_register.style.display = "none";
-            caja_trasera_register.style.display = "flex";
+            caja_trasera_register.style.display = "block";
             caja_trasera_login.style.display = "none";
         }
     }
 
     function register(){
         if (window.innerWidth > 850){
-            formulario_register.style.display = "flex";
-            contenedor_login_register.style.left = "-300px";
+            formulario_register.style.display = "block";
+            contenedor_login_register.style.left = "410px";
             formulario_login.style.display = "none";
             caja_trasera_register.style.opacity = "0";
             caja_trasera_login.style.opacity = "1";
         }else{
-            formulario_register.style.display = "flex";
+            formulario_register.style.display = "block";
             contenedor_login_register.style.left = "0px";
             formulario_login.style.display = "none";
             caja_trasera_register.style.display = "none";
-            caja_trasera_login.style.display = "flex";
+            caja_trasera_login.style.display = "block";
             caja_trasera_login.style.opacity = "1";
         }
 }
 
-    /*function animacion()
-    {
-        if(main-btn.onclick){
-            let start = Date.now();
-
-            let timer = setInterval(function() {
-              let timePassed = Date.now() - start;
-        
-              train.style.left = timePassed / 5 + 'px';
-        
-              if (timePassed > 2000) clearInterval(timer);
-        
-            }, 20);
-        } */
-    
-
-
-
+$(window).scroll(function() {
+    var scrollTop = $(this).scrollTop();
+    var headerToDisplay = false;
+    $('.header--container[data-visible-range]').each(function() {
+        var range = $(this).data('visible-range').split('-');
+        range[0] = range[0] ? parseInt(range[0], 10) : 0;
+        range[1] = range[1] ? parseInt(range[1], 10) : $(document).height();
+        if(scrollTop >= range[0] && scrollTop <= range[1]) {
+            headerToDisplay = $(this);
+            return false;
+        }
+    });
+    if(headerToDisplay && !headerToDisplay.is(':visible')) {
+        $('.header--container[data-visible-range]').hide();
+        headerToDisplay.show();
+    }
+});
