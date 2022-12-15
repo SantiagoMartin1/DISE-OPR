8f028c83da97b78e9490eb3c1a528e91ce2b10a7
MaximoRomano <102158982+MaximoRomano@users.noreply.github.com>
2022-12-15 14:43:18 -0300

    reverted

--- .php/change_user.php
+++ .php/change_user.php
@@ -8,39 +8,35 @@ include("conexion.php");
 
 
 
-// echo $_SESSION["mail"];
+//echo $_SESSION["mail"];
 
 $nombre = $_POST["chng_name"];
 
 
-// if(isset($_POST["btn_name"]))
-// {
+if(isset($_POST["btn_name"]))
+{
 
-    $query4 = mysqli_query($conn,"UPDATE usuario SET Username = '".$nombre."' WHERE correo_electronico = '".$_SESSION["mail"]."'");
-    // $row = mysqli_num_rows($query4);
+    $query4 = mysqli_query($conn,"UPDATE usuario 2022 set Username = '".$nombre."' WHERE correo_electronico = '".$_SESSION["mail"]."'");
+    $row = mysqli_num_rows($query4);
 
-    // var_dump($query4);
-
-    if($query4)
+    if($row = 1)
     {   
-        // echo "bien";
-        //echo "<script> alert ('realizado con exito,$nombre'); window.location = '../DiseOprEn/html-en/config.html' </script>";
-        echo $nombre;
+        echo "<script> alert ('realizado con exito'); window.location = '../DiseOprEn/html-en/config.html' </script>";
     }
     else
     {
-      // echo "<script> alert ('usuario no existe'); window.location = './DiseOprEn/html-en/config.html' </script>"; 
+        echo "<script> alert ('usuario no existe'); window.location = './DiseOprEn/html-en/config.html' </script>"; 
         
-        //   echo "mal"; 
+           
     }
     
-// }
-// else
-// {
-//     //echo "<script> alert ('error'); window.location = './DiseOprEn/html-en/config.html' </script>"; 
-// }
-// echo $_SESSION["mail"];
-//header("Location: ../DiseOprEn/html-en/config.html");
+}
+else
+{
+    echo "<script> alert ('error'); window.location = './DiseOprEn/html-en/config.html' </script>"; 
+}
+
+header("Location: ../DiseOprEn/html-en/config.html");
 
  
 ?>
